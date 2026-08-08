5a9efc6334b61c47ba5f383e64442a3c1867a82f
Pavel.Averochkin <inodoropavel@gmail.com>
2026-08-08 13:47:13 +0000

    correcting client

---
+++ clients/client.cpp
@@ -0,0 +1,43 @@
+/// @file client.cpp
+///
+/// Клиент. Реализация.
+///
+
+
+#include "client.h"
+
+
+Client::Client( ClientId id, std::string name )
+    : id_{ id }
+    , name_{ name }
+    , session_{ }
+{}
+
+ClientId Client::GetId() const
+{
+    return id_;
+}
+
+std::string Client::GetName() const
+{
+    return name_;
+}
+
+void Client::SetSession( SessionWeakPtr session )
+{
+    session_ = session;
+}
+
+SessionId Client::GetSessionId() const
+{
+    if ( !session_.expired() )
+    {
+        return session_.lock()->GetId();
+    }
+    return invalidSessionId;
+}
+
+void Client::Disconnect()
+{
+    session_.reset();
+}
--- clients/client.h
+++ clients/client.h
@@ -1,36 +1,54 @@
 #pragma once
+
 /// @file client.h
 ///
-/// Клиент.
+/// Клиент. Объявление.
 ///
 
 
 #include "../server/session.h"
+#include "../server/session.h"
+#include <cstdint>
 #include <memory>
 #include <optional>
 #include <unordered_set>
 
-using ClientId = uint64_t;  /// ID клиента.
+using ClientId = uint64_t;    /// ID клиента.
+inline uint64_t invalidClientId = 0; /// Невалидный ID клиента.
 
 /// @brief Стуктура клиента.
-struct Client
+class Client
 {
+public:
     /// @brief Конструктор.
     /// @param[in] id ID клиента.
     /// @param[in] name Имя клиента.
     Client( ClientId id, std::string name );
 
+    /// @brief Получение ID клиента.
+    /// @return ID клиента.
     ClientId GetId() const;
 
-    std::string GetName();
+    /// @brief Получение имени клиента.
+    /// @return Имя клиента.
+    std::string GetName() const;
 
+    /// @brief Получение ID сессии клиента.
+    /// @return ID сессии клиента.
+    SessionId GetSessionId() const;
+
+    /// @brief Уставнока сессии клиенту.
+    /// @param[in] session Слабая ссылка на сессию.
+    void SetSession( SessionWeakPtr session );
+
+    /// @brief Сообщение клиенту, что он деавторизован.
     void Disconnect();
 
-    ClientId id;
-    std::string name;
-    std::unordered_set< Topic > subscriptions;
-    std::optional< SessionId > sesId;
-    SessionWeakPtr session; 
+private:
+    ClientId id_;
+    std::string name_;
+    /// std::unordered_set< Topic > subscriptions;
+    SessionWeakPtr session_; 
 };
 
 
