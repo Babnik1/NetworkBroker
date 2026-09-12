96467f1652c65591bd780eeadfffd6501e2c8f44
Pavel.Averochkin <inodoropavel@gmail.com>
2026-09-12 20:05:17 +0000

    fixing build client

--- conanfile.py
+++ conanfile.py
@@ -2,15 +2,12 @@ from conans import ConanFile, CMake
 
 class NetworkBrokerConan( ConanFile ):
     name = "NetworkBroker"
-    version = "1.0.0" # Сделать глобальную перепенную версии.
+    version = "1.0.0" # @todo Сделать глобальную переменную версии.
     settings = "os", "compiler", "build_type", "arch"
 
     generators = "cmake"
 
     def requirements( self ):
-
-        # Пример добавления классической зависимости
-        # self.requires("poco/1.12.4")
         self.requires( "nlohmann_json/3.11.3" )
         self.requires( "boost/1.86.0" )
         self.requires( "gtest/1.15.0")
